a620228715dace6bb54e1ecf77c1f32fc10967a6
Mikhail Yanov <yanovmikhailco@ya.ru>
2023-04-03 10:55:39 +0200

    initial

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { FC, PropsWithChildren } from 'react'
+import HomePage from '../pages/HomePage'
+import './normalize.css'
+import './app.css'
+
+const App: FC<PropsWithChildren> = () => {
+  return <HomePage />
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import { FC, PropsWithChildren } from 'react'
+
+const HomePage: FC<PropsWithChildren> = () => {
+  return <div>HomePage</div>
+}
+
+export default HomePage
